a76c8436bbaa5e223bc850779abc029acf7b4f98
Jay Whitsitt <jay.whitsitt@gmail.com>
2018-04-20 19:59:10 -0500

    Add next meetup function

---
+++ functions/index.js
@@ -0,0 +1,84 @@
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
+// See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
+// for Dialogflow fulfillment library docs, samples, and to report issues
+'use strict';
+
+const functions = require('firebase-functions'); 
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
+
+const axios = require('axios');
+const meetupUrl = 'https://api.meetup.com/GDG-Kansas-City/events?&sign=true&photo-host=public&page=2&status=upcoming&only=id,venue.name,utc_offset,time,name,link';
+
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+
+  function welcome(agent) {
+    agent.add(`Welcome to my agent!`);
+  }
+
+  function fallback(agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+}
+
+   // Uncomment and edit to make your own intent handler
+   // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+   // below to get this function to be run when a Dialogflow intent is matched
+   function nextMeetup(agent) {
+     return axios.get(meetupUrl)
+      .then((res) => {
+        const events = res.data;
+        let prettyResponse = "";
+        if (events.length === 0) {
+          prettyResponse = `There aren't any upcoming events.`;
+        } else {
+          const event = events[0];
+          const name = event.name;
+          const location = event.venue.name;
+          const eventUrl = event.link;
+          prettyResponse = `The next meetup is "` + name + `" at ` + location + `.`;
+          agent.add(new Card({
+            title: name,
+            buttonText: `Open on Meetup.com`,
+            buttonUrl: eventUrl
+          }));
+        }
+
+        agent.add(prettyResponse + ` What else can I help with?`);
+        return;
+      })
+      .catch((err) => {
+        console.log('Error: ' + err);
+        agent.add(`There was an error`);
+      });
+   }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!') // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
+  // // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs/tree/master/samples/actions-on-google
+  // // for a complete Dialogflow fulfillment library Actions on Google client library v2 integration sample
+
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map();
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+   intentMap.set('Next Meetup', nextMeetup);
+  // intentMap.set('your intent name here', googleAssistantHandler);
+  agent.handleRequest(intentMap);
+});
