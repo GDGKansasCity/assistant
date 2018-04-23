1172f4fd09c2d2711b22b1c465ecfa805cd4aca5
Jay Whitsitt <jay.whitsitt@gmail.com>
2018-04-22 22:00:44 -0500

    Add date and image to card

--- functions/index.js
+++ functions/index.js
@@ -24,39 +24,66 @@ const {
   SimpleResponse,
  } = require('actions-on-google');
 const axios = require('axios');
-const meetupUrl = 'https://api.meetup.com/GDG-Kansas-City/events?&sign=true&photo-host=public&page=1&fields=featured_photo&status=upcoming&only=id,venue.name,utc_offset,time,name,link,featured_photo';
+const meetupUrl = 'https://api.meetup.com/GDG-Kansas-City/events?&sign=true&photo-host=public&page=1&fields=featured_photo&only=id,venue,time,name,link,featured_photo.photo_link,description';
+
+const Helpers = require('./helpers');
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
 const app = dialogflow();
 
 app.intent('meetup.next', conv => {
-  console.log('meetup.next without agent');
   return axios.get(meetupUrl)
     .then((res) => {
       const events = res.data;
-      let prettyResponse = "";
+      let response = "";
       let card;
+
       if (events.length === 0) {
-        prettyResponse = `There aren't any upcoming events.`;
+        response = `There aren't any upcoming events.`;
       } else {
         const event = events[0];
-        const name = event.name;
-        const location = event.venue.name;
+        const title = event.name;
+        const description = event.description;
         const eventUrl = event.link;
-        prettyResponse = `The next meetup is "` + name + `" at ` + location + `.`;
+        let photoUrl = event.featured_photo.photo_link;
+
+        let epochTime = event.time + event.utc_offset;
+        let speakableDate = Helpers.formatDateTime(epochTime, 'dddd, MMMM Do');
+        let time = Helpers.formatDateTime(epochTime, 'h:mm a');
+        let speakableDateTime = speakableDate + ' at ' + time;
+        let displayableDate = Helpers.formatDateTime(epochTime, 'dddd M/D');
+        let displaybleDateTime = displayableDate + ', ' + time;
+
+        const venue = event.venue;
+        const venueName = venue.name;
+        const addr1 = venue.address_1;
+        const addr2 = venue.address_2;
+        const city = venue.city;
+        const state = venue.state;
+        const address = Helpers.makeAddress(addr1, addr2, city, state);
+
+        let textOutput = `What else can I help with?`;
+        let speechOutput = `The next meetup is ` + title + ` at ` + venueName + ` on ` + speakableDateTime + `. ` + textOutput;
+        response = new SimpleResponse({text: '', speech: speechOutput});
+
+        // response = `The next meetup is "` + title + `" at ` + venueName + `.`;
         card = new BasicCard({
-          title: name,
-          subtitle: location,
-          text: `Description goes here`,
+          title: title,
+          subtitle: displaybleDateTime + " at " + venueName,
+          text: description,
           buttons: new Button({
-            title: `Open on Meetup.com`,
+            title: 'Open on Meetup.com',
             url: eventUrl
+          }),
+          image: new Image({
+            url: photoUrl,
+            alt: 'Event photo',
           })
         });
       }
 
-      conv.ask(prettyResponse + ` What else can I help with?`);
+      conv.ask(response);
       if (card !== undefined) {
         conv.ask(card);
       }
