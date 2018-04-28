57f5d17da0c5fdb51104b1b7cde044dc61b3c933
Jay Whitsitt <jay.whitsitt@gmail.com>
2018-04-27 23:35:39 -0500

    Add randomizer

--- functions/index.js
+++ functions/index.js
@@ -10,15 +10,34 @@
 
 const functions = require('firebase-functions'); 
 const { dialogflow } = require('actions-on-google');
+
 const axios = require('axios');
-const meetupUrl = 'https://api.meetup.com/GDG-Kansas-City/events?&sign=true&photo-host=public&page=1&fields=featured_photo&only=id,venue,time,utc_offset,name,link,featured_photo.photo_link,description';
+const { randomize, Randomization } = require('randomize');
 
 const Helpers = require('./helpers');
 const Events = require('./events');
+const meetupUrl = 'https://api.meetup.com/GDG-Kansas-City/events?&sign=true&photo-host=public&page=1&fields=featured_photo&only=id,venue,time,utc_offset,name,link,featured_photo.photo_link,description';
 
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-const app = dialogflow();
+class Helper {
+  constructor(conv) {
+    this.conv = conv;
+  }
+
+  askForMore() {
+    this.conv.ask(new Randomization(
+      `What else can I do for you?`,
+      `How else can I help today?`,
+      `What else can I help with?`
+    ));
+  }
+}
+
+const app = dialogflow()
+  .middleware(conv => {
+    conv.helper = new Helper(conv);
+  });
 
 app.intent('meetup.next', conv => {
   return axios.get(meetupUrl)
@@ -30,7 +49,7 @@ app.intent('meetup.next', conv => {
         Events.mentionMeetup(conv, events[0]);
       }
 
-      conv.ask(`What else can I help with?`);
+      conv.helper.askForMore();
       return;
     })
     .catch((err) => {
